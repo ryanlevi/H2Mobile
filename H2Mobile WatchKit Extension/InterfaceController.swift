8371a986dc79269ffa33c7b8101310c1fb8caf1d
Ryan Levi <ryan.e.levi@gmail.com>
2017-09-02 17:06:15 -0700

    Initial Commit

---
+++ H2Mobile WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  H2Mobile WatchKit Extension
+//
+//  Created by Ryan Levi on 9/2/17.
+//  Copyright © 2017 Ryan Levi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
